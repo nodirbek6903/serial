5d3f626b73e2c4b138d175fd9f4e08618a9a3824
nodirchik6903 <nu2913634@gmail.com>
2023-03-01 19:58:14 +0300

    Malumotlar yuborish JS  56-dars

--- js/jsonlash.js
+++ js/jsonlash.js
@@ -1,26 +1,26 @@
-const car = {
-  name: "bmw",
-  color: "red",
-  extra: {
-    ballon: 5,
-    airbag: false,
-  },
-};
+// const car = {
+//   name: "bmw",
+//   color: "red",
+//   extra: {
+//     ballon: 5,
+//     airbag: false,
+//   },
+// };
 
-// const objtoJson = JSON.stringify(car);
-// const jsontoObj = JSON.parse(objtoJson);
-// json.stringify objectdan json ga o'giradi
-// json.parse jsondan objectga o'giradi
+// // const objtoJson = JSON.stringify(car);
+// // const jsontoObj = JSON.parse(objtoJson);
+// // json.stringify objectdan json ga o'giradi
+// // json.parse jsondan objectga o'giradi
 
-// console.log(objtoJson);
-// console.log(jsontoObj);
+// // console.log(objtoJson);
+// // console.log(jsontoObj);
 
-// git add .
-// git commit -m "jsonlash"
-// git push origin main
+// // git add .
+// // git commit -m "jsonlash"
+// // git push origin main
 
-const clone = JSON.parse(JSON.stringify(car));
+// const clone = JSON.parse(JSON.stringify(car));
 
-clone.extra.ballon = 10;
-console.log(car);
-console.log(clone);
+// clone.extra.ballon = 10;
+// console.log(car);
+// console.log(clone);
--- js/script.js
+++ js/script.js
@@ -134,7 +134,7 @@ window.addEventListener("DOMContentLoaded", () => {
       closeModal();
     }
   });
-  const modalTimerId = setTimeout(openModal, 5000);
+  const modalTimerId = setTimeout(openModal, 50000);
 
   function showModalByScroll() {
     if (
@@ -215,4 +215,56 @@ window.addEventListener("DOMContentLoaded", () => {
     ".menu .container",
     "menu__item"
   ).render();
+
+  // Form
+  const forms = document.querySelectorAll("form");
+
+  forms.forEach((form) => {
+    postData(form);
+  });
+
+  const msg = {
+    loading: "Loading",
+    success: "Thanks, for submitting our form",
+    failure: "Something went wrong",
+  };
+
+  function postData(form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const statusMessage = document.createElement("div");
+      statusMessage.textContent = msg.loading;
+      form.append(statusMessage);
+
+      const request = new XMLHttpRequest();
+      request.open("POST", "server.php");
+
+      request.setRequestHeader("Content-Type", "application/json");
+
+      const obj = {};
+
+      const formData = new FormData(form);
+
+      formData.forEach((val, key) => {
+        obj[key] = val;
+      });
+      const json = JSON.stringify(obj);
+
+      request.send(json);
+
+      request.send(formData);
+      request.addEventListener("load", () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          statusMessage.textContent = msg.success;
+          form.reset();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 2000);
+        } else {
+          statusMessage.textContent = msg.failure;
+        }
+      });
+    });
+  }
 });
